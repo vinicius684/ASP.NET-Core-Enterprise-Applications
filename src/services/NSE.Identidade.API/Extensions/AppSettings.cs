4aa4b97917bee36a47d3034580ad29dcb0d36876
Vinícius Henriques <viniciuspinholi6@gmail.com>
2024-08-23 10:22:34 -0300

    API Identidade - Configurações JWT

---
+++ src/services/NSE.Identidade.API/Extensions/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Identidade.API.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
--- src/services/NSE.Identidade.API/Program.cs
+++ src/services/NSE.Identidade.API/Program.cs
@@ -1,8 +1,13 @@
-using Microsoft.Extensions.Configuration;
+using System.Text;
 using NSE.Identidade.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using NSE.Identidade.API.Extensions;
+
 
 //Configura os serviços que a aplicação vai usar, registrando-os no container de DI.
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +20,36 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
                 //.AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();//token para caso precise resetar uma senha, autenticar uma conta recem gerada. Em outras palavras, uma criptografia dentro de um link para te reconhecer
+
+// JWT
+var configuration = builder.Configuration;
+
+var appSettingsSection = configuration.GetSection("AppSettings");//atraves da classe configuration vou obter uma seção do app settings
+builder.Services.Configure<AppSettings>(appSettingsSection);//"pedindo" classe AppSettings represente os dados da seção obtida
+
+var appSettings = appSettingsSection.Get<AppSettings>();//através da section, obtendo a classe já populada
+var key = Encoding.ASCII.GetBytes(appSettings.Secret);//tranformando minha chave em uma sequencia de bytes no formato ASCII
+
+builder.Services.AddAuthentication(options =>//Dizendo que tanto a forma de autenticar, quanto o "desafio" de como apresentar e credenciar um usuário é feito internamente dependem do Padrão JWT, poderia usar via cookie, via outros providers...
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(bearerOptions =>//add suporte  pra esse tipo específico de token e opções
+{
+    bearerOptions.RequireHttpsMetadata = true;
+    bearerOptions.SaveToken = true;
+    bearerOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,//validar o emissor com base na assinatura, não posso utilizar um token qualquer com uma ssinatura qualquer
+        IssuerSigningKey = new SymmetricSecurityKey(key),//assinatura do emissor, segredo do token
+        ValidateIssuer = true,//validar emissor
+        ValidateAudience = true,//validar onde o oken é válido
+        //ValidAudiences =
+        ValidAudience = appSettings.ValidoEm,
+        ValidIssuer = appSettings.Emissor,
+    };
+}); 
+//
 
 builder.Services.AddControllers();
 
