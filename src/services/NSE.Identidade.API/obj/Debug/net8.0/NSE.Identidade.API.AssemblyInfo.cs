a95aacb7f7131d2bed24d6e4cd0dad18fdf2608c
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-01-31 22:09:19 -0300

    API Carrinho - Implementando um gRPC Server ptI

--- src/services/NSE.Identidade.API/obj/Debug/net8.0/NSE.Identidade.API.AssemblyInfo.cs
+++ src/services/NSE.Identidade.API/obj/Debug/net8.0/NSE.Identidade.API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSE.Identidade.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08c218c9d8419fda14d8175e6f772e64726c7b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5cf3c63f4cf6c70dbbf5b166108c28787ae72c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSE.Identidade.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSE.Identidade.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
